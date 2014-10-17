583d84967470c18645cc315c59f836a40e506eb7
Christian Aaron Murga <cmurga4960@gmail.com>
2014-10-17 14:09:31 -0600

    Friday Mereg

--- EPCCFallFestApp/gen/edu/epcc/epccfallfestapp/R.java
+++ EPCCFallFestApp/gen/edu/epcc/epccfallfestapp/R.java
@@ -19,7 +19,9 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int metaButtonBarStyle=0x7f010000;
     }
     public static final class color {
+        public static final int black=0x7f040001;
         public static final int black_overlay=0x7f040000;
+        public static final int red=0x7f040002;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -28,31 +30,83 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ab_background_textured_holoween=0x7f020000;
+        public static final int ab_bottom_solid_holoween=0x7f020001;
+        public static final int ab_solid_holoween=0x7f020002;
+        public static final int ab_stacked_solid_holoween=0x7f020003;
+        public static final int ab_texture_tile_holoween=0x7f020004;
+        public static final int ab_transparent_holoween=0x7f020005;
+        public static final int btn_cab_done_default_holoween=0x7f020006;
+        public static final int btn_cab_done_focused_holoween=0x7f020007;
+        public static final int btn_cab_done_holoween=0x7f020008;
+        public static final int btn_cab_done_pressed_holoween=0x7f020009;
+        public static final int buttonshape=0x7f02000a;
+        public static final int cab_background_bottom_holoween=0x7f02000b;
+        public static final int cab_background_top_holoween=0x7f02000c;
+        public static final int ghosts=0x7f02000d;
+        public static final int ic_launcher=0x7f02000e;
+        public static final int list_focused_holoween=0x7f02000f;
+        public static final int list_pressed_holoween=0x7f020010;
+        public static final int menu_dropdown_panel_holoween=0x7f020011;
+        public static final int monster_hunt_title=0x7f020012;
+        public static final int monster_hunt_title_old=0x7f020013;
+        public static final int monster_icon=0x7f020014;
+        public static final int mummy=0x7f020015;
+        public static final int progress_bg_holoween=0x7f020016;
+        public static final int progress_horizontal_holoween=0x7f020017;
+        public static final int progress_primary_holoween=0x7f020018;
+        public static final int progress_secondary_holoween=0x7f020019;
+        public static final int selectable_background_holoween=0x7f02001a;
+        public static final int spinner_ab_default_holoween=0x7f02001b;
+        public static final int spinner_ab_disabled_holoween=0x7f02001c;
+        public static final int spinner_ab_focused_holoween=0x7f02001d;
+        public static final int spinner_ab_pressed_holoween=0x7f02001e;
+        public static final int spinner_background_ab_holoween=0x7f02001f;
+        public static final int tab_indicator_ab_holoween=0x7f020020;
+        public static final int tab_selected_focused_holoween=0x7f020021;
+        public static final int tab_selected_holoween=0x7f020022;
+        public static final int tab_selected_pressed_holoween=0x7f020023;
+        public static final int tab_unselected_focused_holoween=0x7f020024;
+        public static final int tab_unselected_holoween=0x7f020025;
+        public static final int tab_unselected_pressed_holoween=0x7f020026;
+        public static final int vampire=0x7f020027;
     }
     public static final class id {
-        public static final int fragmentContainer=0x7f080000;
-        public static final int hintText=0x7f080002;
-        public static final int leader1Text=0x7f080006;
-        public static final int leader2Text=0x7f080007;
-        public static final int leader3Text=0x7f080008;
-        public static final int monster_camera_progressContainer=0x7f080009;
-        public static final int monster_camera_surfaceView=0x7f08000a;
-        public static final int monster_camera_takePictureButton=0x7f08000b;
-        public static final int monster_imageView=0x7f080003;
-        public static final int photoButton=0x7f080005;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080004;
+        public static final int action_settings=0x7f090013;
+        public static final int editText1=0x7f090003;
+        public static final int fragmentContainer=0x7f090000;
+        public static final int fragmentExampleContainer=0x7f090001;
+        public static final int gamefrag_register=0x7f090006;
+        public static final int hintText=0x7f090005;
+        public static final int imageView1=0x7f090002;
+        public static final int leader1Text=0x7f09000a;
+        public static final int leader2Text=0x7f09000b;
+        public static final int leader3Text=0x7f09000c;
+        public static final int login_confirm=0x7f09000f;
+        public static final int login_password=0x7f09000e;
+        public static final int login_username=0x7f09000d;
+        public static final int monster_imageView=0x7f090007;
+        public static final int photoButton=0x7f090009;
+        public static final int register_confirm=0x7f090012;
+        public static final int register_password=0x7f090011;
+        public static final int register_username=0x7f090010;
+        public static final int textView1=0x7f090004;
+        public static final int textView2=0x7f090008;
     }
     public static final class layout {
         public static final int activity_fragment=0x7f030000;
-        public static final int fragment_game=0x7f030001;
-        public static final int fragment_monster_camera=0x7f030002;
+        public static final int example_fragment=0x7f030001;
+        public static final int fragment_game=0x7f030002;
+        public static final int login=0x7f030003;
+        public static final int register=0x7f030004;
+        public static final int splash=0x7f030005;
+    }
+    public static final class menu {
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f060008;
         public static final int app_name=0x7f060000;
-        public static final int desc=0x7f060009;
         public static final int hint=0x7f060001;
         public static final int hint_one=0x7f060002;
         public static final int leader_1=0x7f060004;
@@ -60,9 +114,12 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int leader_3=0x7f060006;
         public static final int leaders_board=0x7f060003;
         public static final int photo_button=0x7f060007;
-        public static final int take=0x7f06000a;
     }
     public static final class style {
+        public static final int ActionBar_Solid_Holoween=0x7f070003;
+        public static final int ActionBar_Transparent_Holoween=0x7f070004;
+        public static final int ActionBarTabStyle_Holoween=0x7f070007;
+        public static final int ActionButton_CloseMode_Holoween=0x7f07000a;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -89,6 +146,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f070001;
+        public static final int DropDownListView_Holoween=0x7f070006;
+        public static final int DropDownNav_Holoween=0x7f070008;
+        public static final int PopupMenu_Holoween=0x7f070005;
+        public static final int ProgressBar_Holoween=0x7f070009;
+        public static final int Theme_Holoween=0x7f070002;
+        /**  this style is only referenced in a Light.DarkActionBar based theme 
+         */
+        public static final int Theme_Holoween_Widget=0x7f07000b;
     }
     public static final class styleable {
         /** 
--- EPCCFallFestApp/src/edu/epcc/epccfallfestapp/GameFragment.java
+++ EPCCFallFestApp/src/edu/epcc/epccfallfestapp/GameFragment.java
@@ -1,5 +1,12 @@
 package edu.epcc.epccfallfestapp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
@@ -10,6 +17,8 @@ import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,86 +28,107 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class GameFragment extends Fragment{
-	
+
 	private static final String TAG = "MonsterFragment";
-	private static final int REQUEST_PHOTO = 0;
-	
+
+	// Bar code ID constants.
+	public static final String BC_VAMPIRE = "edu.epcc.fall-fest dracula";
+	public static final String BC_MUMMY = "edu.epcc.fall-fest Mummy";
+	public static final String BC_GHOST = "edu.epcc.fall-fest Ghost";
+
 	private final Game mGame = new Game();
-	private ImageView mPhotoView;
+	private ImageView mMonsterImageView;
 	private Button mPhotoButton;
 	private TextView mHintText;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-	}
-	
+	IntentIntegrator scanIntegrator;
+
 	@Override
 	@TargetApi(Build.VERSION_CODES.GINGERBREAD) 
-	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, final Bundle savedInstanceState){
+		
 		getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		
 		View v = inflater.inflate(R.layout.fragment_game, parent, false);
+		
+		mMonsterImageView = (ImageView)v.findViewById(R.id.monster_imageView);
+		
+		scanIntegrator = new IntentIntegrator(this);
 		
 		mPhotoButton = (Button)v.findViewById(R.id.photoButton);
 		mPhotoButton.setOnClickListener(new View.OnClickListener() {
-			
+		
 			@Override
 			public void onClick(View v) {
-				Intent i = new Intent(getActivity(), MonsterCameraActivity.class);
-				startActivityForResult(i, REQUEST_PHOTO);
+				if(v.getId()==R.id.photoButton){
+					scanIntegrator.initiateScan();
+				}
 			}
 		});
-		
-		// Check to make sure there is an available camera and disable if not.
-		PackageManager pm = getActivity().getPackageManager();
-		boolean hasACamera = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA) ||
-				pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT) || 
-				(Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD && Camera.getNumberOfCameras()
-				  > 0);
-		if(!hasACamera) mPhotoButton.setEnabled(false);
+
+		mMonsterImageView.setImageDrawable(PictureUtils.getScaledDrawable(getActivity(), getResources(), R.drawable.monster_hunt_title));
 		
 		mHintText =(TextView)v.findViewById(R.id.hintText);
 		mHintText.setText(mGame.getCurrentHint().getHint());
 		
-		mPhotoView = (ImageView)v.findViewById(R.id.monster_imageView);
-		
 		return v;
 	}
-	
+
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data){
-		if(resultCode != Activity.RESULT_OK) return;
+		if(data == null) return;
+		IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+			
+		// Fragments and FragmentManager initialization
+		Fragment displayFragment = null;	
 		
-		if(requestCode == REQUEST_PHOTO){
-			String filename = data.getStringExtra(MonsterCameraFragment.EXTRA_PHOTO_FILENAME);
-			if(filename != null){
-				mGame.updatePhoto(new Photo(filename));
-				showPhoto();
+		String scanContent = scanningResult.getContents();
+		String scanFormat = scanningResult.getFormatName();
+		Log.i(TAG,"Barcode content: "+scanContent+"\nBarcode format: "+scanFormat+"\n");
+		int resourceID = 0;
+		
+		if(scanContent.equals(BC_VAMPIRE)){ 
+			resourceID = R.drawable.vampire;
+			displayFragment = new ExampleFragment();
+			mGame.update(BC_VAMPIRE);
 			}
+		else if(scanContent.equals(BC_MUMMY)){ 
+			resourceID = R.drawable.mummy;
+			mGame.update(BC_MUMMY);
 		}
-	}
-	
-	@Override
-	public void onStart() {
-		super.onStart();
-		showPhoto();
-	}
-	
-	@Override
-	public void onStop(){
-		super.onStop();
-		PictureUtils.cleanImageView(mPhotoView);
-	}
-	
-	private void showPhoto(){
-		Photo p = mGame.getCurrentPhoto();
-		BitmapDrawable b = null;
-		if(p != null){
-			String path = getActivity().getFileStreamPath(p.getFilename()).getAbsolutePath();
-			b = PictureUtils.getScaledDrawable(getActivity(), path);
+		else if(scanContent.equals(BC_GHOST)){
+			resourceID = R.drawable.ghosts;
+			mGame.update(BC_GHOST);
 		}
-		mPhotoView.setImageDrawable(b);
+		else resourceID = R.drawable.monster_hunt_title;
+		
+		// TODO: Update all display variables
+		mHintText.setText(mGame.getCurrentHint().getHint());
+		
+		mMonsterImageView.setImageDrawable(PictureUtils.getScaledDrawable(getActivity(), getResources(), resourceID));
+		
+		if(displayFragment != null)
+			getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, displayFragment).addToBackStack(TAG).commit();
 	}
-
 }
+
+
+
+
+
+/*
+Button register = (Button)v.findViewById(R.id.gamefrag_register);
+		if(new File(getActivity().getFilesDir(), "register.txt").exists())
+			register.setVisibility(View.INVISIBLE);
+		else
+		{
+			register.setOnClickListener(new View.OnClickListener()
+			{
+				@Override
+				public void onClick(View v)
+				{
+					Fragment newFragment = new Register();
+					MainActivity.frame.removeAllViews();
+					MainActivity.fm.beginTransaction().add(R.id.fragmentContainer, newFragment).commit();
+				}
+			});
+*/
