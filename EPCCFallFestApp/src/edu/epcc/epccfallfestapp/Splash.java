ba47c06ec008a0b65261cc20355f949ec830ba77
Christian Aaron Murga <cmurga4960@gmail.com>
2014-10-16 18:58:03 -0600

    Stable server

---
+++ EPCCFallFestApp/src/edu/epcc/epccfallfestapp/Splash.java
@@ -0,0 +1,31 @@
+package edu.epcc.epccfallfestapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+
+public class Splash extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		final Context context = this;
+		new CountDownTimer(5000, 5001)
+		{
+			@Override
+			public void onTick(long millisUntilFinished){}
+
+			@Override
+			public void onFinish()
+			{
+				Intent i = new Intent(context, MainActivity.class);
+				startActivity(i);
+				finish();
+			}
+		}.start();
+	}
+}
