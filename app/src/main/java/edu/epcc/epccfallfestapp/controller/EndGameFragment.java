bafe7110f0c52f62913162fa710140415ff2c8b0
Aaron DeWitt <adewitt76@gmail.com>
2015-10-19 12:42:53 -0600

    restructured entire file system

---
+++ app/src/main/java/edu/epcc/epccfallfestapp/controller/EndGameFragment.java
@@ -0,0 +1,48 @@
+/*
+ * EPCC Fall Festival Android app
+ * This application is designed as a scavenger hunt game to 
+ * be deployed and played at the EPCC Fall Festival.
+ * 
+ * File: GameFragment.java
+ * Author: Aaron DeWitt
+ */
+
+package edu.epcc.epccfallfestapp.controller;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.epcc.epccfallfestapp.R;
+
+public class EndGameFragment extends Fragment {
+		
+	private static final String TAG = "EndGameFragment";
+	private MediaPlayer mSongPlayer;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
+		 View v = inflater.inflate(R.layout.end_game_fragment, parent, false);
+		return v;
+	}
+	
+	@Override
+	public void onResume(){
+		super.onResume();
+		mSongPlayer = MediaPlayer.create(getActivity(), R.raw.app_song);
+		mSongPlayer.setLooping(true);
+		mSongPlayer.start();
+	}
+	
+	public void onPause(){
+		super.onPause();
+		if(mSongPlayer != null){
+			mSongPlayer.stop();
+			mSongPlayer.release();
+			mSongPlayer = null;
+		}
+	}
+}
---
+++ app/src/main/java/edu/epcc/epccfallfestapp/controller/MainActivity.java
@@ -0,0 +1,148 @@
+/*
+ * EPCC Fall Festival Android app
+ * This application is designed as a scavenger hunt game to 
+ * be deployed and played at the EPCC Fall Festival.
+ * 
+ * File: MainActivity.java
+ * Author: Aaron DeWitt
+ */
+
+package edu.epcc.epccfallfestapp.controller;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.google.android.gms.*;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+import com.google.example.games.basegameutils.BaseGameUtils;
+
+import edu.epcc.epccfallfestapp.R;
+
+public class MainActivity extends FragmentActivity implements View.OnClickListener,
+		GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+
+	private static final String TAG = "MainActivity";
+
+	// used to establish a connection with Google
+	private GoogleApiClient googleApiClient;
+
+	// The following variables are used when a connection fails
+	private static int RC_SIGN_IN = 9001;
+	private boolean mResolvingConnectionFailure = false;
+	private boolean mAutoStartSignInFlow = true;
+	private boolean mSignInClicked = false;
+
+	public static FragmentManager fm;
+	public static FrameLayout frame;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Create the Google Api Client with access to the Play Games services
+		googleApiClient = new GoogleApiClient.Builder(this)
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this)
+				.addApi(Games.API).addScope(Games.SCOPE_GAMES)
+						// add other APIs and scopes here as needed
+				.build();
+
+
+		setContentView(R.layout.activity_fragment);
+		frame = (FrameLayout)findViewById(R.id.fragmentContainer);
+		fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		if(fragment == null){
+			fragment = new GameFragment();
+			fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+		}
+
+		//findViewById(R.id.badConnectionButton).setOnClickListener(this);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		googleApiClient.connect();
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		googleApiClient.disconnect();
+	}
+
+	// The following are part of the Google APIs
+	@Override
+	public void onConnected(Bundle bundle) {
+		Log.i(TAG, "Connection established");
+		GameFragment gameFragment = ((GameFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainer));
+		if(gameFragment != null) gameFragment.hideBadConnectionBox();
+	}
+
+	@Override
+	public void onConnectionSuspended(int i) {
+
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+
+		if(connectionResult.getErrorCode() == ConnectionResult.SIGN_IN_REQUIRED){
+			Log.e(TAG,"Sign in required");
+		}
+
+		if (mResolvingConnectionFailure) {
+			// Already resolving
+			return;
+		}
+
+		// If the sign in button was clicked or if auto sign-in is enabled,
+		// launch the sign-in flow
+		if (mSignInClicked || mAutoStartSignInFlow) {
+			mAutoStartSignInFlow = false;
+			mSignInClicked = false;
+			mResolvingConnectionFailure = true;
+
+			// Attempt to resolve the connection failure using BaseGameUtils.
+			// The R.string.signin_other_error value should reference a generic
+			// error string in your strings.xml file, such as "There was
+			// an issue with sign in, please try again later."
+			if (!BaseGameUtils.resolveConnectionFailure(this,
+					googleApiClient, connectionResult,
+					RC_SIGN_IN, "Cannot connect to Google APIs")) {
+				mResolvingConnectionFailure = false;
+			}
+		}
+
+		Log.e(TAG,"Connection Failed");
+		GameFragment gameFragment = ((GameFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainer));
+		if(gameFragment != null) {
+			Log.i(TAG,"Found gameFragment");
+			gameFragment.showBadConnectionBox();
+		}
+		else Log.e(TAG,"gameFragment == null");
+	}
+
+	@Override
+	public void onClick(View view) {
+		if(view.getId() == R.id.badConnectionButton) {
+			Log.i(TAG, "badConnectionButton clicked");
+			if(googleApiClient.isConnected()) {
+				GameFragment gameFragment = ((GameFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainer));
+				if(gameFragment != null) gameFragment.hideBadConnectionBox();
+			}
+			else {
+				googleApiClient.disconnect();
+				googleApiClient.connect();
+			}
+		}
+	}
+}
